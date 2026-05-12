256010380be06ef44811e7c318a23d099b8c0bf9
adi7312 <zdrian.alewski@gmail.com>
2026-05-12 22:31:27 +0200

    feat: nn_acc firmate

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include "uart/uart.h"
 #include "interrupts/interrupt.h"
+#include "nn_accelerator/nn_accelerator.h"
 #include "xil_printf.h"
 
 uint8_t frame_buffer[FRAME_SIZE];
 
 int main() {
     if (Init_UART() != XST_SUCCESS) return 1;
+    if (NN_Init() != XST_SUCCESS) return 1;
     if (Init_Interrupts() != XST_SUCCESS) return 1;
 
     xil_printf("=== Firmware Wedrowiec V1.0 ===\r\n");
 
     while (1) {
         UART_ReceiveFrame(frame_buffer);
-        xil_printf("Frame received, sending to processing unit...\r\n");
+        xil_printf("Frame received, sending to FPGA...\r\n");
 
-        // TODO: Next state: transfer data to FPGA
-        // TODO: NN_SendToFifo(frame_buffer); 
+        NN_SendFrame(frame_buffer);
 
-        // Next state: wait
-        while (!fpga_finished) {
-            // TODO: Awaiting interrupt
-        }
+        while (!fpga_finished);
+
         fpga_finished = 0;
 
-        // TODO: Next state: get decision,
-        // TODO: uint32_t decyzja = NN_GetDecision();
-        xil_printf("Processing unit ended its job.\r\n");
+        uint32_t decision = NN_GetDecision();
+        xil_printf("Decision: 0x%08lX\r\n", decision);
     }
 
     return 0;
